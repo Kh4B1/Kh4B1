218ed8100bf36810d8d8ed318e76b8cf8170d69d
HyukJoo <101186824+Kh4B1@users.noreply.github.com>
2022-12-13 13:14:55 +0900

    Delete 졸업작품/API directory

--- 졸업작품/API/Calendar.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs")
-router.get("/", (req,res)=>{
-        res.render('calendar',{
-            isOwner : req.session.isOwner,
-            nick : req.session.nick
-        })})
-module.exports = router
